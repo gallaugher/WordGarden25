53a747575acacaac8d6b7636dd9e520a88fe499b
gallaugher <john.gallaugher@bc.edu>
2025-02-06 12:49:04 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by John Gallaugher on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
